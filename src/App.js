5e6ecbbe471735619ce676d2a971c0e43d6355fa
Alexander Wiederin <alexander.wiederin@gmail.com>
2019-05-30 15:47:23 +0100

    setup axios request and WebSocket connection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App" />
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      tradingPairsInfo: [],
+      selectedPair: 'btcusd',
+      data: null,
+    };
+
+    this.getPairsInfo = this.getPairsInfo.bind(this);
+    this.createWebsocketConnection = this.createWebsocketConnection.bind(this);
+  }
+
+  componentDidMount() {
+    this.getPairsInfo();
+    this.createWebsocketConnection();
+  }
+
+  getPairsInfo() {
+    axios.get('https://www.bitstamp.net/api/v2/trading-pairs-info/')
+      .then((response) => {
+        const { data } = response;
+        this.setState({ tradingPairsInfo: data });
+      });
+  }
+
+  createWebsocketConnection() {
+    const { selectedPair } = this.state;
+    const subscribeMsg = {
+      event: 'bts:subscribe',
+      data: {
+        channel: `order_book_${selectedPair}`,
+      },
+    };
+    const socket = new WebSocket('wss://ws.bitstamp.net');
+    socket.onopen = () => socket.send(JSON.stringify(subscribeMsg));
+    socket.onmessage = (message) => {
+      const { data } = message;
+      this.setState({ data });
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">test</div>
+    );
+  }
 }
 
 export default App;
